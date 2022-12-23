2c2c9d0b965356f250b45ba70c3d8aa536c3d6c0
lkearn <liam@sproutlabs.com.au>
2022-12-23 14:32:28 +1100

    Feat: Add kinda eh day-6 solution, I might try and cleanup the casting.

---
+++ day-6/src/main.rs
@@ -0,0 +1,87 @@
+#![feature(hash_drain_filter)]
+use std::collections::HashMap;
+
+fn main() {
+    for ex in [EXAMPLE_1, EXAMPLE_2, EXAMPLE_3, EXAMPLE_4] {
+        println!("{}", yuck_solve(ex, 4).unwrap());
+    }
+
+    println!("{}", yuck_solve(ACTUAL, 4).unwrap());
+
+    for ex in [EXAMPLE_5, EXAMPLE_6, EXAMPLE_7, EXAMPLE_8, EXAMPLE_9] {
+        println!("{}", yuck_solve(ex, 14).unwrap());
+    }
+
+    println!("{}", yuck_solve(ACTUAL, 14).unwrap());
+}
+
+fn yuck_solve(input: &str, window_size: isize) -> Option<isize> {
+    let mut window: HashMap<char, isize> = HashMap::new();
+
+    for (idx, char) in input.chars().enumerate() {
+        // Because I suck:
+        let idx = idx as isize;
+
+        // Remove the oldest member once the window is full.
+        let pred = idx - window_size;
+        window.drain_filter(|_, v| v == &pred).for_each(drop);
+
+        // Check if we're ready to complete and just return the next idx.
+        if window.len() == (window_size as usize) - 1 && !window.contains_key(&char) {
+            return Some(idx + 1);
+        }
+
+        // Insert into our window updating the idx if duplicitous.
+        window.insert(char, idx);
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        yuck_solve, ACTUAL, EXAMPLE_1, EXAMPLE_2, EXAMPLE_3, EXAMPLE_4, EXAMPLE_5, EXAMPLE_6,
+        EXAMPLE_7, EXAMPLE_8, EXAMPLE_9,
+    };
+
+    #[test]
+    fn test_part_one_examples() {
+        assert_eq!(yuck_solve(EXAMPLE_1, 4), Some(5));
+        assert_eq!(yuck_solve(EXAMPLE_2, 4), Some(6));
+        assert_eq!(yuck_solve(EXAMPLE_3, 4), Some(10));
+        assert_eq!(yuck_solve(EXAMPLE_4, 4), Some(11));
+    }
+
+    #[test]
+    fn test_part_one_actual() {
+        assert_eq!(yuck_solve(ACTUAL, 4), Some(1850));
+    }
+
+    #[test]
+    fn test_part_two_examples() {
+        assert_eq!(yuck_solve(EXAMPLE_5, 14), Some(19));
+        assert_eq!(yuck_solve(EXAMPLE_6, 14), Some(23));
+        assert_eq!(yuck_solve(EXAMPLE_7, 14), Some(23));
+        assert_eq!(yuck_solve(EXAMPLE_8, 14), Some(29));
+        assert_eq!(yuck_solve(EXAMPLE_9, 14), Some(26));
+    }
+
+    #[test]
+    fn test_part_two_actual() {
+        assert_eq!(yuck_solve(ACTUAL, 14), Some(2823));
+    }
+}
+
+const EXAMPLE_1: &str = r#"bvwbjplbgvbhsrlpgdmjqwftvncz"#;
+const EXAMPLE_2: &str = r#"nppdvjthqldpwncqszvftbrmjlhg"#;
+const EXAMPLE_3: &str = r#"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"#;
+const EXAMPLE_4: &str = r#"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"#;
+
+const EXAMPLE_5: &str = r#"mjqjpqmgbljsphdztnvjfqwrcgsmlb"#;
+const EXAMPLE_6: &str = r#"bvwbjplbgvbhsrlpgdmjqwftvncz"#;
+const EXAMPLE_7: &str = r#"nppdvjthqldpwncqszvftbrmjlhg"#;
+const EXAMPLE_8: &str = r#"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"#;
+const EXAMPLE_9: &str = r#"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"#;
+
+const ACTUAL: &str = r#"cdhccdbdggfjjgssjzjzggjnjpnpbbzbnzzflfjfnfrrpvrvbrvvrvggvlvnnbrnrcncsnndbndbnndbdndfdrdvrvvndvvbggnrrnbrnntffgttwzwnnmvmcvvhsstzzlnlwlttbzzpnpmnnjvjnntmnmfftwwrfwrwswmmfrrfrrgbrbffwvvshvhrhmhththbbmqbmqqlslhssrmmqdmmjtmtmjtmjtttnwnvwvqwqjjnbbbdbqbnbpnbnllglcglcgcdczdznnqhhfthtmtlldqlqrrmddrldlzdllvddjcddqfqbqsbqqnllwppqpqzzrbbdppzsppjdpdqpdqdfqfrrwbwrwwqcqcsqsvvpbvbbztzptzzpccdtdhdffvqvcvzzmwzwddjfdffplplqlvlmmmvggpmpvpddpbptplpvlplvpvvnrvnnbqqqjhhwfhwfhwhqhmmpphqpqvppfzpzjzddgzzwffjmjggwhwwnnmlmpmmhcmcpcrcddvzvpzzwnznfznffgdgvddvtvgvsvdsdbbjnjtntbnttgbbbvgvgrgrzrvzrzddlsddcndcnnfqnnmpmlppdlplzplzpzgzmzmddlvlnnbttbwwhbhdhfdfssjppmcpplpdddnpdnnljlwjljsjnjhnhvhvqqsffrbbdttjdjndjdwwsfspffnhfhhlvhvmmqjmmwzwszwwvdvpdvdbdtdsdtsshvvmtvmtmctclchccrllznzfffpjjvhhdmhhvphpghgsgmmhlhnlnmnlnslnlgngznnsqnqddllpwllmzmjmttptfpplglqlgglgqqptqqmvmtmjmddcchbblltslsvsmvmgghmmccnzcztczzmnmttrdrvvcvzvvzllbhllnldndbbqffbbgtgddbtdbbzttdptpccjnjppbllbzlblrlcllhrrhqhgqqbcqcvcdvvnnzfzvzttrptrrwmrmlrlddvttdbtdbdcdvccwlcwwhphmppwfppclpcllgqgnghhvfflfggrzrcchfhhrdhrdhdnhnmmhjjwqjjpmmwvmmdnmnzzqmqwwmtthtdhtthnnqhqdhqqndqqwffsbspbptpmmndnllsmmdhmhfhnhjhghshlslppbgpgngddlsljsjmmzqzhhswhssfzssfqqcmqcmqcmmqggjcjvvgssrccwddmpmwwdfdpdbdpdwdvvqfvfrvvvsbvbhvvmqqcjqqvzqzppncnhhqnnpgplpqqpjpbblpbbbshsthhvfhfmmqzmmznnvrvqrrwdrdlrlwlttzqttjvttqltqqnfqqqwjqwwqttfstftjffsqqnhhnsnqqhggbsgghfgglslmssqlqhlhthqhccdsspsnssshbbnmngnnhllwclcffqllsrszrssnqsqvqjvjcvcttqgqbqmmfqfsqfsqswwvcvffndnfdfvfcvvggsmsfmfwfpfwwzhznntgtlglmmlfmllwrlrwwhcchqchhznzjjcdjdbjjhcjcscwwlnnsgngqqtgqgngnwgnnhqnnhchmchhtchcnclcmccgffbmmzvvrnngwwvddzccnjntjtwjwwztwtmtddjddpsptpbpbvvbwwnlnmndmnmdnnclnnbsbddpfdfvvjtjqqtqqqzjzlzqllzzwwlppvfffpcffffprrncnnzsnznhhwvvqhqphpjjgqqvnnmdmqqglqlblgglrlsspscsjjpvpbpjjwccslsppdjpdjjwvjjmhjhtjjwqqbqjqzjqzqpqbbswwlssqzssbjjpjqjbbjcjpjspjssjjzhhhnjhnhbnhhwzhzwlcshqlqpzgggzmcwntcwmfgtrwwjdpnbdqqcgnzgbdrzdmpwgvtvqffqbpvjpjrcfswffllnvnwvhclpjcwqwgnwqwvwsfgflrgzzsswffwjdjgvdvlgmczcbthwbvhggwzwlzfmhvwvjpbpnhcczbgfhhgghsmjwnvnsvnvmqwstrgnncwbqgbqpgdngllcqnzgwswpgtwzgqzggnzsdgltrlqfctqfvlzdswccfpdtjbfnrbqsmpjclnplbmqbmvwbzzdflwbqrljvzjpcrmnqsmrpqlmfsgcmthqpwgwzvmrjnhqczljcpnzjbwzrhjrzmcqpmlbzhgmqrlzsjbjsvcmcngptzlrthwsrjrlmsrgjlzrvpzwmprwnpgvjtspsppfvwfwcvbnqcwwmzlbqthqmbnbmnsnzgsbbnqtrvhlzjhphclpjzrdblszrnftqgwwrhpznhjhgrncvsvrmtmmgssvzddjfrnrzhbrqrfffjvzrqdnrdbvjwgrvlcpbncfgczlwdggsjmwzhndcdbggjvwfljctjnsjwczwfdrfttbhnlswfdbpcnwpspdhnzwqbgdswwpccbpfpgmfmvvwpzbzqsbbjbfnhjpszcbnrdplmwtdjtpcsztdjcmczltnstzwlcdbtdhsdgsgtlvdfqggfmmrppjfrmtfwhpbjsppszjbhmthndqmvbmqcbtqsltwrcvlvblwspbgspjftwllzcmnsrvjpnstzrfmcflnhppsdfggwbzvnvlnjqlfvrlplnzvfrwvgcgqvnpfgtgchctvhcplclzmfpwgnfhqjgglfmsgpflqcpqmbbhwnvvdllcnhblpnndbdtmgvfbvvvlvzlrpfqmnvzbfrssjtlgcjtpfznshvdjrjnfshfcgvwcdbqlfsbhnzwmsgwhpbzttgfjsqgwvdmbdwjljhsndrbbzfrsqjhcbldzqpmtnfvnmzltjcrvrltwshnhqlnclmcnfpbzstsczlqmfmdftzfbcwqnhqppzfbzpbfjhmmtvtbmblmtshsbtjlvsqvmbmgstbbdmhprqmtpfdqqntmnlbmpsmwfgrvstjcllhwpcddnljdjvdrbwqmgrjnldpgnrhgqpzqrvwzsngrgnbpjnsffzjsbdptwnnfcqlscfhvggpfstsnqzcjbqqhgdpqsrlprcppgqmddpqpbnvgwtdqsbbgtvsqfrtqfsbdzhsztfmvwrrsjcbtcjgzrnhnpgldtwbwgmwbgmjjzsbbzlhgmlczrhjwtzrgwscmjvlstprldhglvftqzbtrmcwzgtjppbnjcdvjvcwvdbngnbrmjvvtnwdqfclbpgzcfnnnlnngtgmhsqsdmbjctjzjpbrwrhscqshmmwbtfnzjgsrjlnqqdsvdrjdzsdprphnfmwwcztqfcrjvnfhlvnqwbrfmcvhrbtgvcrqjjfcnzwmlfzzdcbbzvphhmsdltwjfdcgthpvszqzjdbfwrpvhbjqdhrscnvjhjvvcldnhgjclmzpbrrwnscgpcqrpdgsnjnwhctcdqgwqbrcszfzpmtdrhlftvwffdjrtznqrppqbdbwvzmtlpvsqqpcngjgfdrpngnspdwhhvlhqrtsphgqrlldggtrvqsprbfdmrpgcmqphdvjfmhlznpgtqlvtnllcdhzhhtjjlfvdlwhcrfmjmdjtmbllvsfgvmfqtqlmrlrjmqptszvjdpzhphppljnpjdjpwlrclssgdnstchhwhpflmlrtdqvqbbljrmnflrltzpqmgqfrczvfzrpfsrwsgpljvjfjdjdvjchcdmmtjgghqspwzdtwqgtvmnrrbfbgnhcrvnzznrdlqmgmdwmpwzlqdjtvpszwnjtjtmjqvfwvftlhgpvgzswpbvbllfcwpjnsmbhzrdpdzjsrpnhphdcqjmzvvhrjcwhgwjwcshqwzpbpmfnjjvqcjrqmvsrdrtdvfhwhrbpvrqrsfzflslqtdrtcsggtzmpvbszdgttlvpwwltvpcwqmnwqtpcfzgsvsmncvpqqdrljfwtncplmjlpfcnqmcctwzhrbmrfwvsrjsbnhjrjmrnbmmnnhsvlltwzzhsgwppnlmljgpcsmpchdjdzpgvrtwsfzffhnlbfmrldzbshvpqhnfzpwnvczgfvhbntcpztwqlfgtsmdhvcrgjhvqrhbpvbpzcpbgzrcfjztbnfjptbzfpztwprwf"#;
